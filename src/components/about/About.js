36d5824c828a77c07be657d30c559b83b26b9a5f
Rafaelleo93 <rafaelleonardo93@gmail.com>
2022-08-07 19:14:26 +0200

    about first view

---
+++ src/components/about/About.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./About.css"
+
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className="about-container">
+        <div className="about-desc">
+        <h3>Let me tell you somthing about me</h3>
+        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>
+        </div>
+        <div className="about-img"> 
+        <img src="https://blog.ida.cl/wp-content/uploads/sites/5/2015/01/179_1_adhamdannaway.com-webdesign-inspiration-website-clean-creative-655x470.jpg" alt=""></img>
+        </div>
+    </div>
+  )
+}
+
+export default About
