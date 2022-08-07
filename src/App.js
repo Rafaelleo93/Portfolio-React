af991bcdc40251d9c0f3a6122032d1ab80a015ff
Rafaelleo93 <rafaelleonardo93@gmail.com>
2022-08-07 19:47:08 +0200

    slider first view

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import Cover from "./components/cover/Cover";
 import Navbar from "./components/navbar/Navbar";
+import About from "./components/about/About"
+import Slider from "./components/slider/Slider"
 
 
 function App() {
@@ -20,6 +22,8 @@ function App() {
     <div className="App">
       <Navbar isScrolling={scrollHeight} />
       <Cover />
+      <About />
+      <Slider />
     </div>
   );
 }
---
+++ src/components/slider/Slider.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Slider.css"
+import Carousel from "@brainhubeu/react-carousel"
+
+
+import React from 'react'
+
+const Slider = () => {
+  return (
+    <div>Slider</div>
+  )
+}
+
+export default Slider
