38da56de4d24423c855406b4ae32b7c9f124bf30
Rafaelleo93 <rafaelleonardo93@gmail.com>
2022-08-07 18:58:01 +0200

    navbar first view

--- src/components/cover/Cover.js
+++ src/components/cover/Cover.js
@@ -6,7 +6,8 @@ const Cover = () => {
   return (
     <div className= "cover-container">
         <video className= "video" src= {CoverVideo}autoPlay loop mute />
-        
+        <h1>Name Last Name</h1>
+        <p>Developer | Designer | Content Creator | Entrepeneur</p>
         </div>
   )
 }
